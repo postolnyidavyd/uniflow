cc72dd6e0444cf5caaf0fa0eb9f1a40dc559ad00
postolnyidavyd <postolnyi.davyd@student.uzhnu.edu.ua>
2026-04-03 16:09:55 +0300

    Add wallet service

--- uniflow_backend/Services/Wallet/WalletService.cs
+++ uniflow_backend/Services/Wallet/WalletService.cs
@@ -1,24 +1,101 @@
+using DataAccess.Data;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Services.Wallet;
 
 public class WalletService : IWalletService
 {
-    public Task CreateWallet(Guid userId)
+    private readonly AppDbContext _appDbContext;
+
+    public WalletService(AppDbContext appDbContext)
     {
-        throw new NotImplementedException();
+        _appDbContext = appDbContext;
+    }    
+    public async Task CreateWallet(Guid userId)
+    {
+        var wallet = new StudentWallet() { UserId = userId };
+
+        await _appDbContext.StudentWallets.AddAsync(wallet);
+
+        await _appDbContext.SaveChangesAsync();
+        
     }
 
-    public Task<int> GetBalance(Guid userId)
+    public async Task<int> GetBalance(Guid userId)
     {
-        throw new NotImplementedException();
+        var wallet = await _appDbContext.StudentWallets.FirstOrDefaultAsync(sw => sw.UserId == userId);
+        if(wallet == null)
+            throw new KeyNotFoundException("Не існує користувача з таким id");
+        return wallet.Balance;
     }
 
-    public Task ChargeTokens(Guid userId, int amount, string reason)
+    public async Task ChargeTokens(Guid userId, int amount, string? description)
     {
-        throw new NotImplementedException();
+        await using var transaction = await _appDbContext.Database.BeginTransactionAsync();
+        try
+        {
+            var wallet = await _appDbContext.StudentWallets
+                .FirstOrDefaultAsync(sw => sw.UserId == userId);
+            if(wallet == null)
+                throw new KeyNotFoundException("Гаманець не знайдено");
+            
+            wallet.Balance += amount;
+            
+            var tokenTransaction = new TokenTransaction()
+                { Amount = amount, Description = description, StudentWalletId = wallet.Id };
+            await _appDbContext.TokenTransactions.AddAsync(tokenTransaction);
+
+            await _appDbContext.SaveChangesAsync();
+            await transaction.CommitAsync();
+        }
+        catch
+        {
+            await transaction.RollbackAsync();
+            throw;
+        }
     }
 
-    public Task SpendTokens(Guid userId, int amount, string reason)
+    public async Task SpendTokens(Guid userId, int amount, string? description)
     {
-        throw new NotImplementedException();
+        
+        await using var transaction = await _appDbContext.Database.BeginTransactionAsync();
+        try
+        {
+            var wallet = await _appDbContext.StudentWallets
+                .FirstOrDefaultAsync(sw => sw.UserId == userId);
+            if(wallet == null)
+                throw new KeyNotFoundException("Гаманець не знайдено");
+            if (wallet.Balance < amount)
+                throw new ArgumentException("Недостатньо токенів");
+            
+            wallet.Balance -= amount;
+            
+            var tokenTransaction = new TokenTransaction()
+                { Amount = -amount, Description = description, StudentWalletId = wallet.Id };
+            await _appDbContext.TokenTransactions.AddAsync(tokenTransaction);
+
+            await _appDbContext.SaveChangesAsync();
+            await transaction.CommitAsync();
+        }
+        catch
+        {
+            await transaction.RollbackAsync();
+            throw;
+        }
     }
+
+    // public async Task RecalculateBalance(Guid userId)
+    // {
+    //     var wallet = await _appDbContext.StudentWallets.FirstOrDefaultAsync(sw => sw.UserId == userId);
+    //     if(wallet == null)
+    //         throw new ArgumentException("Не існує користувача з таким id");
+    //     
+    //     var tokenAmount = await _appDbContext.TokenTransactions.Where(tt => tt.StudentWalletId == wallet.Id)
+    //         .SumAsync(tt => tt.Amount);
+    //     if (tokenAmount < 0)
+    //         tokenAmount = 0;
+    //     wallet.Balance = tokenAmount;
+    //     await _appDbContext.SaveChangesAsync();
+    // }
 }
